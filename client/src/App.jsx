894133d80fe76f5d76be684c65624604317669ba
Rekonir <zhed-nikita@mail.ru>
2023-12-18 12:49:22 +0300

    proto-auth

---
+++ client/src/App.jsx
@@ -0,0 +1,60 @@
+import { useContext, useEffect, useState } from "react";
+import LoginForm from "./components/LoginForm";
+import { Context } from "./main";
+import { observer } from "mobx-react-lite";
+import UserService from "./services/UserService";
+
+function App() {
+  const { store } = useContext(Context);
+  const [users, setUsers] = useState([]);
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.checkAuth();
+    }
+  }, []);
+
+  if (store.isLoading) {
+    return <p>Загрузка...</p>
+  }
+  if (!store.isAuth) {
+    return (
+      <>
+        <LoginForm />
+        <button onClick={getUsers}>Получить список пользователей</button>
+        {users.map((user) => (
+          <h2 key={user.email}>{user.email}</h2>
+        ))}
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>
+        {store.isAuth
+          ? `Пользователь ${store.user.email} авторизован`
+          : `Добро пожаловать в прототип`}
+      </h1>
+      <h2>
+        {store.user.isActivated 
+          ? `Почта ${store.user.email} подтверждена`
+          : `Пожалуйста, подтвердите свою почту`}
+      </h2>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <button onClick={getUsers}>Получить список пользователей</button>
+      {users.map((user) => (
+        <h2 key={user.email}>{user.email}</h2>
+      ))}
+    </>
+  );
+}
+
+export default observer(App);
---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+const router = require('./router/index')
+const errorMiddleware = require('./middlewares/error-middleware')
+
+
+const PORT = process.env.PORT || 5000;
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL
+}))
+app.use('/api', router)
+app.use(errorMiddleware)
+
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL)
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start()
